42e3d14e471043bef75338499aa4a43d9312dacc
Udhayan K <kudhayan27@gmail.com>
2026-03-15 23:21:15 +0530

    IMPL: Added router for UDP connection and gossipsib for chatroom

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ use iroh::{Endpoint, SecretKey};
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    let secret_key: SecretKey = SecretKey::generate{rand::rng::OsRng}; 
-    println!("> our secret key: {secret_key}");
+    let secret_key= SecretKey::generate(rand::rngs::OsRng); 
+    println!("> our secret key: {}",secret_key);
     
-    let endpoint = Endpoint::builder().discovery_n0().bind().await?;
+    let endpoint = Endpoint::builder()
+        .secret_key(secret_key)
+        .discovery_n0()
+        .bind()
+        .await?;
+    println!("> our node id is: {}",endpoint.node_id());
     
+    let gossip = Gossip::new();
+    
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn()
+        .await?;
+    Ok(())
 }
