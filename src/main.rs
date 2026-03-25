88f271df43fa1500924f2b1c7e3d576b4fbdde06
Udhayan K <kudhayan27@gmail.com>
2026-03-25 23:57:30 +0530

    Setting up router for connection

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use anyhow::Result;
-use iroh::{Endpoint, SecretKey};
+use iroh::{Endpoint, key::SecretKey, protocol::Router};
+use iroh_gossip::net::Gossip;
+use std::sync::Arc;
 
 #[tokio::main]
-async fn main() -> Result<()>{
-    let secret_key= SecretKey::generate(rand::rngs::OsRng); 
-    println!("> our secret key: {}",secret_key);
+async fn main() -> anyhow::Result<()> {
+    let secret_key = SecretKey::generate();
+    let pubic_key = secret_key.public();
+    println!("P2P Chat ...");
+    println!("Node ID: {}",pubic_key);
+    
     
     let endpoint = Endpoint::builder()
         .secret_key(secret_key)
         .discovery_n0()
         .bind()
         .await?;
-    println!("> our node id is: {}",endpoint.node_id());
     
-    let gossip = Gossip::new();
+    
+    
+    let gossip = Gossip::spawn(endpoint.clone()).await?;
     
     let router = Router::builder(endpoint.clone())
-        .accept(iroh_gossip::ALPN, gossip.clone())
+        .accept(iroh_gossip::ALPN,gossip.clone())
         .spawn()
         .await?;
+    
+    println!("Network Layer Ready");
+    
+    tokio::signal::ctrl_c().await?;
+    router.shutdown().await?;    
+    
     Ok(())
 }
